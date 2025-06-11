101c43b6f27e828733f20abf4c23c1c5bd267d13
Simona Koprivnjak <simonakoprivnjak@gmail.com>
2025-06-11 17:59:11 +0200

    new 10.6

---
+++ main.js
@@ -0,0 +1,13 @@
+const readMore = document.querySelector(".read-more");
+const ccContent = document.querySelector(".cc-container");
+console.log(readMore);
+
+readMore?.addEventListener('click', () => {
+ ccContent?.classList.toggle('expanded');
+ if(ccContent.classList.contains("expanded")){
+    readMore.innerHTML = 'Read Less';
+ } else {
+    readMore.innerHTML = 'Read More';
+ }
+})
+window.alert("Would you like to sign up for our newsletter?");
